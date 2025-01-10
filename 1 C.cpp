26046098496132cc11fa5f8ecb2ad394c975b39b
Feni275 <fenimustika275@gmail.com>
2025-01-10 16:14:02 +0700

    Modul 8

--- 1 C.cpp
+++ 1 C.cpp
@@ -14,27 +14,6 @@ int main() {
         A[count] = input;
         count++;
     }
-    
-    printf("a. A:\n");
-    for (int i = 0; i < count; i++) {
-        printf("%d ", A[i]);
-    }
-    printf("\n");
-
-    int B[11];
-    int b_count = 0;
-    for (int i = 0; i < count; i++) {
-        if (A[i] > 9) {
-            B[b_count] = A[i];
-            b_count++;
-        }
-    }
-
-    printf("b. A (nilai > 9):\n");
-    for (int i = 0; i < b_count; i++) {
-        printf("%d ", B[i]);
-    }
-    printf("\n");
 
     int C[11];
     int c_count = 0;
@@ -48,14 +27,6 @@ int main() {
     printf("c. A (nilai ganjil):\n");
     for (int i = 0; i < c_count; i++) {
         printf("%d ", C[i]);
-    }
-    printf("\n");
-    
-    printf("d. A (ganjil dengan tata letak):\n");
-    for (int i = 0; i < count; i++) {
-        if (A[i] % 2 != 0) {
-            printf("%d ", A[i]);
-        }
     }
     printf("\n");
 
